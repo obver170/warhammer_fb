dd48c5c247d1985657bbf9e1012e271c45b385a0
obver170 <obver170@gmail.com>
2021-07-23 16:02:52 +0300

    finish intelligence

--- skill/admin.py
+++ skill/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
-from .models import Gamble, SkillList, OutdoorSurvival, TrainingOfAnimals, Knowledge, BookSearches, Treatment
+from .models import ProfessionalSkillList, OtherSkillList, Gamble, OutdoorSurvival, TrainingOfAnimals, Knowledge, \
+    BookSearches, Treatment, Veterinarian, Grade, SecretSigns, Language
 
 # Register your models here.
 
+admin.site.register(ProfessionalSkillList)
+admin.site.register(TrainingOfAnimals)
+admin.site.register(Knowledge)
+admin.site.register(BookSearches)
+admin.site.register(Treatment)
+admin.site.register(Veterinarian)
+admin.site.register(Grade)
+admin.site.register(SecretSigns)
+admin.site.register(Language)
 
+
+
+
+
+
+admin.site.register(OtherSkillList)
 admin.site.register(Gamble)
-admin.site.register(SkillList)
 admin.site.register(OutdoorSurvival)
--- skill/models.py
+++ skill/models.py
@@ -77,7 +77,7 @@ class Knowledge(BaseSkill):
         verbose_name_plural = 'Знания'
 
 
-class BookSearches (BaseSkill):
+class BookSearches(BaseSkill):
     base_attribute = 'Интеллект'
     name_skill = 'Книжные изыскания'
     type_skill = 'Профессиональный'
@@ -87,7 +87,7 @@ class BookSearches (BaseSkill):
         verbose_name_plural = 'Книжные изыскания'
 
 
-class Treatment (BaseSkill):
+class Treatment(BaseSkill):
     base_attribute = 'Интеллект'
     name_skill = 'Лечение'
     type_skill = 'Профессиональный'
@@ -97,7 +97,7 @@ class Treatment (BaseSkill):
         verbose_name_plural = 'Лечение'
 
 
-class Veterinarian (BaseSkill):
+class Veterinarian(BaseSkill):
     base_attribute = 'Интеллект'
     name_skill = 'Обращение с животными'
     type_skill = 'Профессиональный'
@@ -107,25 +107,98 @@ class Veterinarian (BaseSkill):
         verbose_name_plural = 'Обращение с животными'
 
 
-class SkillList(models.Model):
-    name_list = models.CharField(max_length=20, verbose_name='Название набора', default='Стандарт')
+class Grade(BaseSkill):
+    base_attribute = 'Интеллект'
+    name_skill = 'Оценка'
+    type_skill = 'Профессиональный'
+
+    class Meta:
+        verbose_name = 'Оценка'
+        verbose_name_plural = 'Оценка'
+
+
+class SecretSigns(BaseSkill):
+    specialization = (
+        ('Бродяги', 'Бродяги'),
+        ('Воры', 'Воры'),
+        ('Охотники', 'Охотники'),
+        ('Разведчики', 'Разведчики'),
+        ('Серый орден', 'Серый орден'),
+        ('Гильдия', 'Гильдия'),
+    )
+
+    special = models.CharField(max_length=25, verbose_name='Специализация', choices=specialization, default=0)
+
+    base_attribute = 'Интеллект'
+    name_skill = 'Тайные знаки'
+    type_skill = 'Профессиональный'
+
+    class Meta:
+        verbose_name = 'Тайные знаки'
+        verbose_name_plural = 'Тайные знаки'
+
+
+class Language(BaseSkill):
+    specialization = (
+        ('Боевой арго', 'Боевой арго'),
+        ('Бретонский', 'Бретонский'),
+        ('Воровской арго', 'Воровской арго'),
+        ('Гильдейский арго', 'Гильдейский арго'),
+        ('Классический', 'Классический'),
+        ('Кхазалид', 'Кхазалид'),
+        ('Магический', 'Магический'),
+        ('Тилейский', 'Тилейский'),
+    )
+
+    special = models.CharField(max_length=25, verbose_name='Специализация', choices=specialization, default=0)
+
+    base_attribute = 'Интеллект'
+    name_skill = 'Язык'
+    type_skill = 'Профессиональный'
+
+    class Meta:
+        verbose_name = 'Язык'
+        verbose_name_plural = 'Язык'
+
+
+class OtherSkillList(models.Model):
+    name_list = models.CharField(max_length=20, verbose_name='Название набора', default='Общие навыки')
+
     gamble = models.ForeignKey(Gamble, on_delete=models.CASCADE, verbose_name=Gamble.name_skill)
     outdoorSurvival = models.ForeignKey(OutdoorSurvival, on_delete=models.CASCADE,
                                         verbose_name=OutdoorSurvival.name_skill, null=True)
-    trainingOfAnimals = models.ForeignKey(TrainingOfAnimals, on_delete=models.CASCADE,
-                                          verbose_name=TrainingOfAnimals.name_skill, null=True, blank=True)
-    knowledge = models.ForeignKey(Knowledge, on_delete=models.CASCADE, verbose_name=Knowledge.name_skill,
-                                  null=True, blank=True)
-    bookSearches = models.ForeignKey(BookSearches, on_delete=models.CASCADE, verbose_name=BookSearches.name_skill,
-                                       null=True, blank=True)
-    treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE, verbose_name=Treatment.name_skill,
-                                       null=True, blank=True)
-    veterinarian = models.ForeignKey(Veterinarian, on_delete=models.CASCADE, verbose_name=Veterinarian.name_skill,
-                                       null=True, blank=True)
 
     def __str__(self):
         return self.name_list
 
     class Meta:
-        verbose_name = 'Набор навыков'
-        verbose_name_plural = 'Наборы навыков'
+        verbose_name = 'Набор общих навыков'
+        verbose_name_plural = 'Наборы общих навыков'
+
+
+class ProfessionalSkillList(models.Model):
+    name_list = models.CharField(max_length=20, verbose_name='Название набора', default='Профессиональные навыки')
+
+    trainingOfAnimals = models.ForeignKey(TrainingOfAnimals, on_delete=models.CASCADE,
+                                          verbose_name=TrainingOfAnimals.name_skill, null=True, blank=True)
+    veterinarian = models.ForeignKey(Veterinarian, on_delete=models.CASCADE, verbose_name=Veterinarian.name_skill,
+                                     null=True, blank=True)
+    knowledge = models.ForeignKey(Knowledge, on_delete=models.CASCADE, verbose_name=Knowledge.name_skill,
+                                  null=True, blank=True)
+    bookSearches = models.ForeignKey(BookSearches, on_delete=models.CASCADE, verbose_name=BookSearches.name_skill,
+                                     null=True, blank=True)
+    treatment = models.ForeignKey(Treatment, on_delete=models.CASCADE, verbose_name=Treatment.name_skill,
+                                  null=True, blank=True)
+    grade = models.ForeignKey(Grade, on_delete=models.CASCADE, verbose_name=Grade.name_skill,
+                              null=True, blank=True)
+    secretSigns = models.ForeignKey(SecretSigns, on_delete=models.CASCADE, verbose_name=SecretSigns.name_skill,
+                                    null=True, blank=True)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, verbose_name=Language.name_skill,
+                                    null=True, blank=True)
+
+    def __str__(self):
+        return self.name_list
+
+    class Meta:
+        verbose_name = 'Набор профессиональных навыков'
+        verbose_name_plural = 'Наборы профессиональных навыков'
